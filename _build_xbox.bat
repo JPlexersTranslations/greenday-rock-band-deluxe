890b46f90557ee92e07d1af3be56e1b96cb8ff72
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-02-04 14:02:08 -0600

    Update _build_xbox.bat

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -2,7 +2,7 @@ git pull https://github.com/hmxmilohax/greenday-rock-band-deluxe main
 @echo OFF
 echo:Make sure your vanilla ark files are in _build/Xbox/gen/
 del "%~dp0\_build\Xbox\gen\main_xbox.hdr"
-del "%~dp0\_build\Xbox\gen\main_xbox_11.ark"
+del "%~dp0\_build\Xbox\gen\main_xbox_10.ark"
 xcopy "%~dp0\_build\_rebuild_files\main_xbox.hdr" "%~dp0\_build\Xbox\gen" >nul
 echo:
 echo:Temporarily moving PS3 files out of the ark path to reduce final ARK size
@@ -12,7 +12,7 @@ echo:Temporarily moving PS3 files out of the ark path to reduce final ARK size
 echo:
 echo:Building Green Day Rock Band Deluxe patch arks.
 echo:The "Unhandled exception" below is expected, and does not indicate failure.
-"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main.hdr" >nul
+"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main_xbox.hdr" >nul
 echo:
 echo:Moving back PS3 files
 @%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.milo_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
